86dd9b85358588d54e32ca65a11d91d8b6a575c3
Toni Santandreu <santandreutoni@gmail.com>
2025-02-06 00:00:22 +0100

    Create trigger build workflow

--- product/product.go
+++ product/product.go
@@ -1,10 +1,13 @@
 package product
 
+import "time"
+
 type Product struct {
 	name        string
 	description string
 	category    string
 	units       int
+	createdAt   time.Time
 }
 
 func (p *Product) AddUnits() {
